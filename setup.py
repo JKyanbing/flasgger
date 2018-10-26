701f4da7ec5aaf3bc9c5f3a2a6a313bc9a9c4560
JKyanbing <904617356@qq.com>
2018-10-26 11:49:21 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def grep(attrname):
 
 
 setup(
-    name='flaswagger',
+    name='flasgger',
     version=grep('__version__'),
     url='https://github.com/JKyanbing/flasgger/',
     license='MIT',
